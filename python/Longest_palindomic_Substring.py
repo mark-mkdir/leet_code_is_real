0db6182a6ba93724e08bbc679ede5ea57f603dcb
mark-mkdir <mark.st1105@gmail.com>
2026-06-21 04:46:57 +0700

    Delete longest palindromic substring solution

--- python/Longest_palindomic_Substring.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        
-        longest = ""
-
-        for i in range(len(s)) :
-            for j in range(i+1,len(s)):
-                if
